71a69e2f0b83755fd00ff752dae7cc66d38c76fd
Thiago F. Peçanha <shorumi@gmail.com>
2020-11-21 03:30:30 -0300

    Remove unnecessary dataSources:
    - Removes dataSources from ApolloServer
    setup settings, because it is injected in
    NasaServicesImpl

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import "dotenv-safe/config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import { NasaApiDataSource } from "./data-sources/NasaApiDataSource";
 import { ApodResolver } from "./resolvers/ApodResolver";
+import { Container } from "typedi";
 
 (async () => {
   const app = express();
@@ -12,18 +12,17 @@ import { ApodResolver } from "./resolvers/ApodResolver";
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [ApodResolver],
+      container: Container,
       validate: true
     }),
     tracing: true,
-    dataSources: () => ({
-        nasaApiDataSource: new NasaApiDataSource()
-    }),
     context: ({ req, res }) => ({ req, res })
   });
 
   apolloServer.applyMiddleware({ app, cors: false });
+  
   const port = process.env.NODE_PORT || 4000;
   app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}/graphql`);
+    console.log(`🚀 server started at http://localhost:${port}/graphql`);
   });
 })();
