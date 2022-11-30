f91a33507e13e16b4cd40146ecc7ef501c68e957
Thiago Felipe Peçanha <thiago@augment.co>
2022-11-29 21:18:44 -0300

    Remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata";
 import "dotenv-safe";
-// import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { buildSchema } from "type-graphql";
@@ -8,16 +7,12 @@ import { ApodResolver } from "./resolvers/ApodResolver";
 import { Container } from "typedi";
 
 (async () => {
-  // const app = express();
-
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [ApodResolver],
       container: Container,
       validate: true
     })
-    // tracing: true,
-    // context: ({ req, res }) => ({ req, res })
   });
   
   const { url } = await startStandaloneServer(
@@ -26,10 +21,6 @@ import { Container } from "typedi";
       listen: { port: 4000 || process.env.NODE_PORT },
     }
   );
-
-  // apolloServer.applyMiddleware({ app, cors: false });
   
-  // app.listen(port, () => {
-    console.log(`🚀 server started at ${url}`);
-  // });
+  console.log(`🚀 server started at ${url}`);
 })();
